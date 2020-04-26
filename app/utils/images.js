759279a28c9b9cda7ff0c643e9652458cb2495e9
Alberto Luebbert M <alberto@ideashappy.com>
2020-04-26 15:41:15 -0500

    Images. Add assets for app

---
+++ app/utils/images.js
@@ -0,0 +1,19 @@
+/* Pokemon */
+import backPokedex from '../assets/pokedex.png';
+import ash from '../assets/ash.png';
+import brook from '../assets/brook.png';
+import misty from '../assets/misty.png';
+import avatar_ash from '../assets/avatar_ash.png';
+import avatar_brook from '../assets/avatar_brook.png';
+import avatar_misty from '../assets/avatar_misty.png';
+
+/* Export Assets */
+export default {
+  backPokedex,
+  ash,
+  brook,
+  misty,
+  avatar_ash,
+  avatar_brook,
+  avatar_misty
+};
