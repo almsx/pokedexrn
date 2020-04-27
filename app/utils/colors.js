0e623c17f7ef6992ae5bd98985bcc3ac3d20becf
Alberto Luebbert M <alberto@ideashappy.com>
2020-04-27 04:56:50 -0500

    Library colors added

---
+++ app/utils/colors.js
@@ -0,0 +1,6 @@
+export const black = '#000000';
+export const white = '#FFFFFF';
+export const btnApp = '#0D47A1';
+export const blueApp = '#0D47A1';
+export const redApp = '#F36';
+export const colorHeader = '#ee1f24';
